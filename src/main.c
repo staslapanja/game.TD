c20535ccd3ecb2d2030a320c89696167532df447
Stas Lapanja <lapanjastas@yahoo.com>
2017-11-01 15:54:36 +0100

    Change font
    Replace font with an open license font

--- src/main.c
+++ src/main.c
@@ -51,7 +51,7 @@ void init(void)
     if (!al_init_image_addon())
         abort_game("Failed to initialize allegro image");
 
-    fonts[0] = al_load_ttf_font("fonts/terminator_real_nfi.ttf",16,0 );
+    fonts[0] = al_load_ttf_font("fonts/orbitron-medium.otf",16,0 );
 
     al_register_event_source(event_queue, al_get_mouse_event_source());
     al_register_event_source(event_queue, al_get_keyboard_event_source());
